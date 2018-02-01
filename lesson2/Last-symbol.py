4ba668d864295659f425e74613d16ab14cca863e
Katadoka <katadoka@gmail.com>
2018-02-01 19:18:30 +0200

    lesson2

--- lesson2/Last-symbol.py
+++ lesson2/Last-symbol.py
@@ -1,11 +1,11 @@
 # The user enters the tape. The program should take the last letter, and all the same letters from the tape. The result is displayed on the screen.
 
-text = input(str('Enter words: '))
+text = input('Enter words: ')
 lastSymbol = text[-1]
 
 new_text = ""
 
-for lastSymbol in str(text):
+for lastSymbol in text:
 	if lastSymbol == text[-1]:
 		continue
 	new_text = new_text + lastSymbol
---
+++ lesson2/sequence_of_numbers.py
@@ -0,0 +1,8 @@
+# Print all numbers from 1 to the user input.
+
+number = int(input('Enter number: '))
+
+n = 0 
+while n < number:
+	n = n + 1
+	print(n)
