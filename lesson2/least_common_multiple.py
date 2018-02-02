51a865c4775bedcad5b2ad381aec71ad8263a48d
Katadoka <katadoka@gmail.com>
2018-02-02 14:48:48 +0200

    lesson2

---
+++ lesson2/least_common_multiple.py
@@ -0,0 +1,9 @@
+# Output the least common multiple of two numbers entered from the keyboard.
+
+number1 = input('Enter first number: ')
+number2 = input('Enter second number: ')
+
+a = 0
+
+for a in number1:
+	b = 
