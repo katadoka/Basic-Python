e0030263ac7fdb4c4f095a3be60b9d440e8fe33c
Katadoka <katadoka@gmail.com>
2018-02-05 16:39:18 +0200

    lesson3

---
+++ lesson3/func_factorial.py
@@ -0,0 +1,10 @@
+# the function that returns the factorial of the number.
+
+def factor (number):
+	if number == 1:
+		return number
+	else:
+		return number*factor(number - 1)
+
+number = int(input('Enter the number: '))
+print (factor(number))
