d1a8149c8d050a7ad9078d96bc9e30150e3a7cb6
Katadoka <katadoka@gmail.com>
2018-02-06 15:57:41 +0200

    lesson2

--- lesson2/least_common_multiple.py
+++ lesson2/least_common_multiple.py
@@ -4,11 +4,11 @@ number1 = int(input('Enter first number: '))
 number2 = int(input('Enter second number: '))
 
 def lcm(num_1, num_2): 
-	if num_1 < num_2: 
-		num_1 , num_2 = num_2, num_1 
-	i=1 
-	while (num_1 * i )% num_2 != 0: 
-		i +=1 
-	return num_1 * i 
+	if num_1 < num_2:
+		num_1 , num_2 = num_2, num_1
+	i=1
+	while (num_1 * i )% num_2 != 0:
+		i +=1
+	return num_1 * i
 
-print(LCM(number1, number2))
+print(lcm(number1, number2))
