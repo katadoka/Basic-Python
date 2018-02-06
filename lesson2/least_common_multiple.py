9947e9769ae6943e090f5fbb0b9c2e9d2160f911
Katadoka <katadoka@gmail.com>
2018-02-06 15:51:56 +0200

    lesson2

--- lesson2/least_common_multiple.py
+++ lesson2/least_common_multiple.py
@@ -1,5 +1,14 @@
 # Output the least common multiple of two numbers entered from the keyboard.
 
-number1 = input('Enter first number: ')
-number2 = input('Enter second number: ')
+number1 = int(input('Enter first number: '))
+number2 = int(input('Enter second number: '))
 
+def lcm(num_1, num_2): 
+	if num_1 < num_2: 
+		num_1 , num_2 = num_2, num_1 
+	i=1 
+	while (num_1 * i )% num_2 != 0: 
+		i +=1 
+	return num_1 * i 
+
+print(LCM(number1, number2))
