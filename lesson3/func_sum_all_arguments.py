c868cd303e700d7df8b49c66ab1f4e3e95c9029c
Katadoka <katadoka@gmail.com>
2018-02-06 13:21:30 +0200

    lesson3

---
+++ lesson3/func_sum_all_arguments.py
@@ -0,0 +1,9 @@
+# the function will return the sum of all arguments.
+
+def sum_args(*args):
+    return sum(int(i) for i in args)
+
+
+assert sum_args(1) == 1
+assert sum_args(1,2,3,4) == 10
+assert sum_args(10, 15) == 25
