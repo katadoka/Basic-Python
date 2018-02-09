abc3add05c0784f5e028b4b0ec1d2ea406b4952f
Katadoka <katadoka@gmail.com>
2018-02-09 17:21:26 +0200

    lesson4

--- lesson4/plant.py
+++ lesson4/plant.py
@@ -15,6 +15,7 @@ class Plant:
 	def __init__(self):
 		self.cars = []
 		self.counter = 0
+		self.dont_work = 0
 	
 	def add_car(self, car):
 		self.cars.append(car)
@@ -27,20 +28,23 @@ class Plant:
 		Plant.counter -= 1
 		
 	def show_info_car(self):
-		print('The total cars of plant: {}'.format(self.counter))
+		print('The total  cars of plant: {}'.format(self.counter))
 		print('The total cars of plants: {}'.format(Plant.counter))
 		for car in self.cars:
 			car.info_car()
-			
+			if car.condition != 'work':
+				self.dont_work += 1
+		print('The total cars of plant is dont work: {}'.format(self.dont_work))
+
 c = Car('BMV', '15000$', 'work')
 c1 = Car('Mersedes', '12000$', 'dont work')
 c2 = Car('Lanos', '3000$', 'dont work')
+
 
 plant = Plant()
 plant.add_car(c)
 plant.add_car(c1)
 plant.add_car(c2)
-plant.show_info_car()
 plant.del_car(c1)
 plant.show_info_car()
 
@@ -49,3 +53,4 @@ plant1.add_car(c)
 plant1.add_car(c1)
 plant1.add_car(c2)
 plant1.show_info_car()
+
