3c728934c469fa32cf04f0bec9f9a6e84a0bf39d
Katadoka <katadoka@gmail.com>
2018-02-18 14:32:15 +0200

    lesson5

--- lesson5/plant.py
+++ lesson5/plant.py
@@ -1,4 +1,6 @@
-# Write a program in the OOP style to emulate the work of the plant. The program will have two classes: for machines and for the plant. Machines have: name, price and information or collected. The plant keeps a list of machines. The plant keeps a list of machines. The plant provides information on how much cars are now in it and how many of them are collected. Also display information on all machines.
+# Write a program in the OOP style to emulate the work of the plant. The program will have two classes: for machines and for the plant. 
+# Machines have: name, price and information or collected. The plant keeps a list of machines. The plant keeps a list of machines. 
+# The plant provides information on how much cars are now in it and how many of them are collected. Also display information on all machines.
 
 class Car:
 	
@@ -9,8 +11,8 @@ class Car:
 		self.condition = condition
 
 	@property 	
-	def info_car(cls):
-		return('{} - {}'.format(cls.price, cls.condition))
+	def info_car(self):
+		return('{} - {}'.format(self.price, self.condition))
 
 	def description(self):
 		print('{} - {}'.format(self.brand, self.info_car))
@@ -29,40 +31,33 @@ class Lanos(Car):
 
 
 class Plant:
-	counter = 0
+	counter = 0 
 	def __init__(self):
 		self.cars = []
-		self.counter = 0
-		self.dont_work = 0
 
 	def add_car(self, car):
-		self.cars.append(car)
-		self.counter += 1
-		Plant.counter += 1
-		for car in self.cars:	
-			if len(self.cars) > 3:
+		if len(self.cars) == 3:
 				print('The plant is full!')
-				return
+		else:
+			self.cars.append(car)
+			Plant.counter += 1
 
 	def del_car(self, car):
 		self.cars.remove(car)
-		self.counter -= 1
 		Plant.counter -= 1
 
 	def assamble(self, car):
 		car.condition = True
 
 	def show_info_car(self):
+		dont_work = 0
 		for car in self.cars:	
-			if len(self.cars) > 3:
-				print('The plant is full!')
-				break
-			elif car.condition != True:
-				self.dont_work += 1
+			if car.condition != True:
+				dont_work += 1
 			car.description()
-		print('The total  cars of plant: {}'.format(self.counter))	
-		print('The total cars of plant is dont work: {}'.format(self.dont_work))
-		print('The total cars of plants: {}'.format(Plant.counter))	
+		print('Total cars: {}'.format(Plant.counter))
+		print('On plant: {}/{}'.format(dont_work, len(self.cars)))	
+
 
 c = BMV('15000$', True)
 c1 = Mersedes('12000$', False)
@@ -81,7 +76,6 @@ plant1.add_car(c)
 plant1.add_car(c1)
 plant1.assamble(c1)
 plant1.add_car(c2)
-plant1.assamble(c2)
 plant1.show_info_car()
 
 plant2 = Plant()
