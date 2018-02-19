a86ed153100a04ee7d8cb6950b10384c97050331
Katadoka <katadoka@gmail.com>
2018-02-19 16:45:59 +0200

    lesson7

--- lesson7/last_elem.py
+++ lesson7/last_elem.py
@@ -1,7 +1,9 @@
 # Write in the last element of the matrix line the sum of all the previous line items using List comprehension 
 # and functional programming.
+from functools import reduce
 
-a = ([0,1,2], [4,3,2])
+a = ([0,1,2,4,3,2])
 
-def last_elem_sum(a):
-	pass
+a1 = a[:-1]
+a1.append(reduce(lambda x, y: x + y,a))
+print(a1)
--- lesson7/sum _of_ numbers.py
+++ lesson7/sum _of_ numbers.py
@@ -1,1 +1,7 @@
 # 	Find the sum of all positive numbers that have 2 digits
+
+from functools import reduce
+
+a = [2, 3, 89, 54, 321, 321, 43 ,12]
+
+print(sum(list(filter(lambda x: x > 9 and x < 100, a))))
