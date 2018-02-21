c96905469fd72242775a29bc280e7e1d02845ce8
Katadoka <katadoka@gmail.com>
2018-02-21 17:36:15 +0200

    lesson1

--- lesson1/calc.py
+++ lesson1/calc.py
@@ -3,17 +3,16 @@
 number1 = int(input("Enter numder: "))
 symbol = input("Enter action: ")
 number2 = int(input("Enter numder: "))
-     
+
 result = 0
-     
+
 if symbol == "+":
-    	result = number1 + number2
+    result = number1 + number2
 elif symbol == "-":
-    	result = number1 - number2
+    result = number1 - number2
 elif symbol == "*":
-    	result = number1 * number2
-elif symbol == "/":	
-    	result = number1 / number2
-     
-print (result)
-       
+    result = number1 * number2
+elif symbol == "/":
+    result = number1 / number2
+
+print(result)
