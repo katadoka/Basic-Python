e1010754c5cab44ba8266713437796f8e63e2d17
Katadoka <katadoka@gmail.com>
2018-02-21 18:27:47 +0200

    lesson5

--- lesson5/plant.py
+++ lesson5/plant.py
@@ -1,63 +1,68 @@
-# Use the program from the previous lesson as a basis. Add the following features:
-# - add different classes of machines. The name does not need to be transmitted, it is given in the class itself.
+# Use the program from the previous lesson as a basis.
+# Add the following features:
+# - add different classes of machines.
+# The name does not need to be transmitted,
+# it is given in the class itself.
 # - add the maximum number of machines at the factory.
-# - add function "assamble", which will accept the car as a parameter and collect it.
+# - add function "assamble", which will accept
+# the car as a parameter and collect it.
+
 
 class Car:
-	
-	brand = None
+    brand = None
 
-	def __init__(self, price, condition):
-		self.price = price
-		self.condition = condition
+    def __init__(self, price, condition):
+        self.price = price
+        self.condition = condition
 
-	@property 	
-	def info_car(self):
-		return('{} - {}'.format(self.price, self.condition))
+    @property
+    def info_car(self):
+        return('{} - {}'.format(self.price, self.condition))
 
-	def description(self):
-		print('{} - {}'.format(self.brand, self.info_car))
+    def description(self):
+        print('{} - {}'.format(self.brand, self.info_car))
 
 
 class BMV(Car):
-	brand = 'BMV'
+    brand = 'BMV'
 
 
 class Mersedes(Car):
-	brand = 'Mersedes'
+    brand = 'Mersedes'
 
 
 class Lanos(Car):
-	brand = 'Lanos'
+    brand = 'Lanos'
 
 
 class Plant:
-	counter = 0 
-	def __init__(self):
-		self.cars = []
+    counter = 0
 
-	def add_car(self, car):
-		if len(self.cars) == 3:
-				print('The plant is full!')
-		else:
-			self.cars.append(car)
-			Plant.counter += 1
+    def __init__(self):
+        self.cars = []
 
-	def del_car(self, car):
-		self.cars.remove(car)
-		Plant.counter -= 1
+    def add_car(self, car):
+        if len(self.cars) == 3:
+                print('The plant is full!')
+        else:
+            self.cars.append(car)
+            Plant.counter += 1
 
-	def assamble(self, car):
-		car.condition = True
+    def del_car(self, car):
+        self.cars.remove(car)
+        Plant.counter -= 1
 
-	def show_info_car(self):
-		dont_work = 0
-		for car in self.cars:	
-			if car.condition != True:
-				dont_work += 1
-			car.description()
-		print('Total cars: {}'.format(Plant.counter))
-		print('On plant: {}/{}'.format(dont_work, len(self.cars)))	
+    def assamble(self, car):
+        car.condition = True
+
+    def show_info_car(self):
+        dont_work = 0
+        for car in self.cars:
+            if car.condition != True:
+                dont_work += 1
+            car.description()
+        print('Total cars: {}'.format(Plant.counter))
+        print('On plant: {}/{}'.format(dont_work, len(self.cars)))
 
 
 c = BMV('15000$', True)
@@ -85,4 +90,3 @@ plant2.add_car(c1)
 plant2.add_car(c2)
 plant2.add_car(c3)
 plant2.show_info_car()
-
